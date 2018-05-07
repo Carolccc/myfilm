2e7189a4a4a8f1b267ea2e060e3fe5cc3cf39fe4
maplez <aa694256235@qq.com>
2018-05-07 10:13:05 +0800

    maple

--- src/components/flim.vue
+++ src/components/flim.vue
@@ -1,12 +1,327 @@
 <template>
   <div class="content">
     <div class="header">
-      <div class="ranking">
-        <el-tabs v-model="activeName" :tab-position="tabPosition" @tab-click="handleClick">
-          <el-tab-pane label="热映榜" name="first"></el-tab-pane>
-          <el-tab-pane label="最受期待榜" name="second">配置管理</el-tab-pane>
-          <el-tab-pane label="TOP10榜" name="third">角色管理</el-tab-pane>
-        </el-tabs>
+      <div class="tags">
+        <ul class="tags-pane">
+          <li class="tags-line">
+            <div class="tags-title">类型：</div>
+              <ul>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+              </ul>
+          </li>
+           <li class="tags-line">
+            <div class="tags-title">类型：</div>
+              <ul>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+              </ul>
+          </li>
+           <li class="tags-line">
+            <div class="tags-title">类型：</div>
+              <ul>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+                 <li class="tags-item">
+                  <a href="#">全部</a>
+                </li>
+              </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div class="movie-list">
+        <dl>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+          <dd>
+            <router-link to="item">
+              <div class="item">
+                <div class="movie-title">
+                  <span class="item-title">头号玩家</span>
+                   <i class="item-sorce">9.5</i>
+                </div>
+              </div>
+            </router-link>
+          </dd>
+        </dl>
       </div>
     </div>
   </div>
@@ -14,37 +329,96 @@
 
 <script>
 export default {
-  data () {
-    return {
-      activeName: 'first',
-      tabPosition: 'left'
-    };
+  data() {
+    return {};
   },
-  methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    }
-  }
-}
-
+  methods: {}
+};
 </script>
 
 <style scoped>
-.content{
+a {
+  text-decoration: none;
+  color: black;
+}
+a:hover {
+  color: red;
+}
+a:active {
+  background: red;
+  color: white;
+}
+dd {
+  margin: 30px 30px 20px 40px;
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+}
+.content {
   margin-top: 63px;
 }
-.header{
+.header {
   width: 1200px;
   margin: 0 auto;
 }
-.ranking{
+.tags {
+  border: 1px solid #e5e5e5;
+  padding: 0 20px;
   margin-top: 100px;
 }
-.el-tab-pane{
-  text-align: center;
+.tags-pane {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0px;
 }
-.el-tabs .el-tabs__item:hover{
-  padding-bottom:50px;
-      color: #404040;
+.tags-line {
+  margin-top: 13px;
+  padding-bottom: 13px;
+  border-bottom: 1px dotted #e5e5e5;
+}
+.tags-title {
+  height: 24px;
+  line-height: 24px;
+  float: left;
+  color: #999;
+  font-size: 14px;
+}
+.tags-item {
+  list-style-type: none;
+  border-radius: 14px;
+  padding: 3px 9px;
+  display: inline-block;
+  margin-left: 5px;
+}
+
+.item-title {
+  color: white;
+  font-size: 18px;
+  width: 160px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-left: 8px;
+}
+.movie-title {
+  width: 160px;
+  bottom: 12px;
+  position: absolute;
+  background: slategray;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.item-sorce {
+  font-size: 18px;
+  color: #fdb863;
+  margin-right: 10px;
+  float: right;
+}
+.item {
+  position: relative;
+  border: 1px solid #efefef;
+  margin: -1px;
+  height: 225px;
+  width: 160px;
+  background: url(../image/hotfilm1.jpg);
+  background-repeat: no-repeat;
+  background-size: 160px 225px;
 }
 </style>
